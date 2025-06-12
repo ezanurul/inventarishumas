6a8ec9c3e807cddb978ef84ba8aeaf4ab07a930e
ezanurul <ezanurulumi.jpg@gmail.com>
2025-06-12 11:36:29 +0700

    tambah seeder akun

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class SuperAdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'admininven@utdi.com',
+            'password' => 'admin123',
+            'role' => 'admin',
+        ]);
+    }
+}
--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -17,3 +17,4 @@ class BarangSeeder extends Seeder
         }
     }
 }
+
