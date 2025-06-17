a61bc64dd742a3c344594b6ba30d1c7355010b0e
ezanurul <ezanurulumi.jpg@gmail.com>
2025-06-17 10:49:26 +0700

    Menambahkan form input barang

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -2,7 +2,23 @@
 @section('content')
 <div class="container">
     <h3>Data Barang</h3>
-    <table class="table table-bordered">
+
+    <!-- Form Input Barang -->
+    <form action="{{ route('barang.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="nama_barang" class="form-label">Nama Barang</label>
+            <input type="text" class="form-control" name="nama_barang" required>
+        </div>
+        <div class="mb-3">
+            <label for="jumlah" class="form-label">Jumlah</label>
+            <input type="number" class="form-control" name="jumlah" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+    </form>
+
+    <!-- Tabel Barang -->
+    <table class="table table-bordered mt-4">
         <thead>
             <tr>
                 <th>No</th>
